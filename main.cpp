867e12cb9507c9b0c5e32a0c5f5fd5267e078769
Slevin Kelevra <SlevinKelevra1980@gmail.com>
2026-04-10 09:57:52 +0200

    - moved draw font to separate function - added constants file - added arial.ttf file to project

--- main.cpp
+++ main.cpp
@@ -2,26 +2,36 @@
 #include <iostream>
 #include <string>
 
+#include "constants.h"
+
+void draw_text(const std::string textToDisplay, const sf::Vector2f &pos, const sf::Color &color, sf::RenderWindow &window)
+{
+    sf::Font font;
+    if (!font.openFromFile("arial.ttf"))
+    {
+        std::cerr << "Could not load font\n";
+        return;
+    }
+
+    sf::Text text(font);
+    text.setString(textToDisplay);
+    text.setCharacterSize(24);
+    text.setFillColor(color);
+    text.setPosition(pos);
+
+    window.draw(text);
+}
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "FPS Counter");
-    window.setFramerateLimit(120);
+    sf::RenderWindow window(sf::VideoMode({ constants::window_width, constants::window_height}), "Breakout Clone");
+    window.setFramerateLimit(60);
 
     sf::RectangleShape rec(sf::Vector2f(20.f, 100.f));
     rec.setFillColor(sf::Color::Blue);
     rec.setPosition({ 100.f, 100.f });
 
-    sf::Font font;
-    if (!font.openFromFile("C:/Windows/Fonts/Arial.ttf"))
-    {
-        std::cerr << "Could not load font\n";
-        return 1;
-    }
-
-    sf::Text text(font);
-    text.setCharacterSize(24);
-    text.setFillColor(sf::Color::White);
-    text.setPosition({ 20.f, 20.f });
+    
 
     std::uint64_t counter = 0;
     sf::Clock totalClock;
@@ -46,11 +56,10 @@ int main()
             fps = static_cast<float>(counter) / seconds;
         }
 
-        text.setString("fps = " + std::to_string(fps));
 
         window.clear();
         window.draw(rec);
-        window.draw(text);
+        draw_text("fps = " + std::to_string(fps), { 20.0f, 20.0f }, sf::Color::White, window);
         window.display();
     }
 }
